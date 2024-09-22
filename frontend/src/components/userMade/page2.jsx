a73cab818f2ddf13aee4a6c17fe352ec7cae073f
Arun <arunchandra738@gmail.com>
2024-09-22 15:48:07 +0530

    Login and Signup working

---
+++ frontend/src/components/userMade/page2.jsx
@@ -0,0 +1,29 @@
+const Page2 = () => {
+  return (
+    <>
+    
+    <div className='flex flex-col items-center mt-[11vw]'> 
+    <h1 className="text-[#3565EC] font-bold text-[3vw]">Tired of <span className='text-yellow-500'>Manual Quizzes?</span></h1>
+    <p className='text-[1.2vw]'>Try automated AI generated quality quizzes to improve your performance with any topic, and have a quiz battle with your friend</p>
+  </div>
+
+
+    <div className="flex mt-[5vw] flex-row gap-[2.5vw] w-[90%] justify-center mx-auto max-w-screen h-[50vw]">
+
+        <div className="w-[24%] h-[40%] bg-[#3764EF] rounded-3xl">
+            
+        </div>
+        <div className="w-[24%] h-[40%] bg-[#FE6376] rounded-3xl">
+            
+        </div>
+        <div className="w-[24%] h-[40%] bg-[#01DBAB] rounded-3xl">
+            
+        </div>
+    </div>
+    
+    </>
+
+  )
+}
+
+export default Page2
